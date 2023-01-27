4d0ce1c652124019b20d53eae071a5fc2a08787b
JakubBurandt <77541270+JakubBurandt@users.noreply.github.com>
2023-01-27 17:48:55 +0100

    Update admin.php

--- admin/admin.php
+++ admin/admin.php
@@ -1,12 +1,14 @@
 <?php
 include('../cfg.php');
+$_SESSION['logged_in'] = false;
+
 function FormularzLogowania()
 {
     $wynik = '
     <div class="logowanie">
         <h1 class="heading">Panel CMS:</h1>
             <div class="logowanie">
-                <form method="post" name="LoginForm" enctype="multipart/form-data" action="'.$_SERVER['REQUEST_URI'].'">
+                <form method="post" name="LoginForm" enctype="multipart/form-data" action="' . $_SERVER['REQUEST_URI'] . '">
                     <table class="logowanie">
                         <tr><td class="log4_t">[email]</td><td><input type="text" name="login_email" class="logowanie" /></td></tr>
                         <tr><td class="log4_t">[haslo]</td><td><input type="password" name="login_pass" class="logowanie" /></td></tr>
@@ -18,41 +20,87 @@ function FormularzLogowania()
     ';
     return $wynik;
 }
-FormularzLogowania()
 
-    $query = "SELECT * FROM page_list WHERE id='$id_clear' ORDER BY data DESC LIMIT 10";
-    $result = mysqli_query($link, $query);
 
-    while($row = mysqli_fetch_array($result))
-    {
-        $row['id'].' '.$row['tytul'].' <br />';
+echo Logowanie(); 
+
+
+function Logowanie()
+{
+    
+    $login = $_SESSION['login'];
+    $pass = $_SESSION['pass'];
+    if (isset($_POST['login_email']) && isset($_POST['login_pass'])) {
+        if ($_POST['login_email'] == $login && $_POST['login_pass'] == $pass) {
+            $_SESSION['logged_in'] = true;
+            ListaPodstron();
+        } else {
+            echo "Błąd logowania. Spróbuj ponownie.<br>";
+            echo FormularzLogowania();
+            
+        }
+    } else {
+        if (!$_SESSION['logged_in']) {
+            echo FormularzLogowania();
+        } else {
+            ListaPodstron();
+        }
     }
-
+}
 
 
 function ListaPodstron()
 {
-    $dbhost = 'localhost';
-    $dbuser = 'root';
-    $dbpass = '';
-    $baza   = 'moja_strona';
+    $link = $_SESSION['link'];
+    $query = "SELECT * FROM page_list ORDER BY id LIMIT 10";
+    $result = mysqli_query($link, $query);
 
-    $conn = mysqli_connect($dbhost , $dbuser, $dbpass, $baza);
-
-    $zapytanie_sql = 'SELECT * FROM page_list';
-    $wyniki = mysqli_query($conn, $zapytanie_sql);
-    
-    $button = '
-    <form method="post" name="button">
-        <input type="button" class="btn btn-light" value="Usuń"/>
-        <input type="button" class="btn btn-light" value="Edytuj"/>
-    </form>
-    ';
-
-    while($wiersz = mysqli_fetch_array($wyniki)) {
-    echo 'id= ' .  $wiersz['id'] . ' page_title= ' . $wiersz['page_title'];
-    echo $button;
+    while ($row = mysqli_fetch_array($result)) {
+        echo $row['id'] . ' ' . $row['page_title'] . '
+        <form method="post" action="'.$_SERVER['PHP_SELF'].'">
+        <button onclick="UsunPodstrone(' . $row['id'] . ')">Usuń</button>
+        <input type="submit" value="Edytuj" name="edit"> <br/><br/>
+        </form>';
     }
- 
 }
+
+
+function EdytujPodstrone($id){
+    $link = $_SESSION['link'];
+    $sql = "SELECT * FROM page_list WHERE id = $id LIMIT 1";
+    $result = mysqli_query($link, $sql);
+    $page = mysqli_fetch_array($result);
+
+    echo '
+    <form method="post">
+    <input type="hidden" name="id" value="' . $page['id'] . '">
+    Tytuł: <input type="text" name="title" value="' . $page['page_title'] . '"><br>
+    Treść: <textarea name="content">' . $page['page_content'] . '</textarea><br>
+    Aktywna: <input type="checkbox" name="status"';
+    if ($page['status'] == 1) {
+        echo 'checked';
+    }
+    echo '<input type="submit" value="Zapisz zmiany">
+    </form>';
+}
+
+
+if(isset($_POST['submit']))
+{
+    EdytujPodstrone('1');
+}
+
+
+function UsunPodstrone($id) {
+    $link = $_SESSION['link'];
+    if(isset($_SESSION['logged_in'])) {
+        $sql = "DELETE FROM pages WHERE id = $id";
+        mysqli_query($link, $sql);
+        echo "Podstrona usunięta";
+    } else {
+        echo "Nie jesteś zalogowany. <a href='admin.php'>Zaloguj</a>";
+    }
+}
+
+
 ?>
